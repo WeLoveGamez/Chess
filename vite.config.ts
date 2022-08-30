63789dbed82f8ec8dc1f0ec57789326c34aceb01
TimL135 <t.littschwager@mbd-team.de>
2022-08-30 21:00:50 +0200

    more pwa stuff

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,22 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
       },
+      manifest: {
+        background_color: '#000000',
+        theme_color: '#7E1F86',
+        name: 'autoCheese',
+        short_name: 'autoCheese',
+        start_url: '/autoCheese',
+        display: 'standalone',
+        // icons: [
+        //   {
+        //     src: "/autoCheese/assets/cheese.1670ace1.png",
+        //     sizes: "144x144",
+        //     type: "image/png",
+        //     purpose: "any",
+        //   },
+        // ],
+      },
     }),
   ],
   base: './',
