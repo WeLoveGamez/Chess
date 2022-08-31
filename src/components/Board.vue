cbb17e585971d7dfe13b1b866ccd40b19f2eeec7
Newkiller <robocraft.tier5@outlook.de>
2022-09-01 00:18:57 +0200

    check if possible and dont blunder

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -59,7 +59,7 @@ import { computed } from '@vue/reactivity';
 import { ref } from 'vue';
 import { board, Tile, applyMove, moveHistory, King1Checked, King2Checked, PIECES, getPieceValue } from '../board';
 import { Position } from '../types';
-import { checkLegalMoves, checkAllLegalMoves } from '../moves';
+import { checkLegalMoves, checkAllLegalMoves, checkChecks } from '../moves';
 import { Button } from 'custom-mbd-components';
 const selectedCell = ref<Position>([-1, -1]);
 const playerTurn = ref<1 | 2>(1);
@@ -119,53 +119,96 @@ function getGoodBotMove(moveableBotPieces: Position[]) {
 
   let targetablePieces: Move[] = [];
   //get targetable Pieces
-  for (let position of moveableBotPieces) {
-    let targets = checkLegalMoves(position[0], position[1], board.value, true).filter(m => board.value[m[0]][m[1]].type);
-    targetablePieces.push({ piece: position, targets: targets });
+  {
+    for (let position of moveableBotPieces) {
+      let targets = checkLegalMoves(position[0], position[1], board.value, true).filter(m => board.value[m[0]][m[1]].type);
+      targetablePieces.push({ piece: position, targets: targets });
+    }
   }
   targetablePieces = targetablePieces.filter(m => m.targets.length != 0);
 
   // filter for free pieces
-  takeBlunders = [...targetablePieces];
-  let enemyMoves: Position[] = [];
-  for (let move of takeBlunders) {
-    for (let target of move.targets) {
-      enemyMoves.push(
-        ...checkAllLegalMoves(
-          applyMove(
-            move.piece[0],
-            move.piece[1],
-            target[0],
-            target[1],
-            checkLegalMoves(move.piece[0], move.piece[1], board.value, true),
-            board.value
-          ),
-          botPlayer.value == 1 ? 2 : 1
-        )
-      );
-      move.targets = move.targets.filter(t => !enemyMoves.find(m => t[0] == m[0] && t[1] == m[1]));
+  {
+    takeBlunders = [...targetablePieces];
+    let enemyMoves: Position[] = [];
+    for (let move of takeBlunders) {
+      for (let target of move.targets) {
+        enemyMoves.push(
+          ...checkAllLegalMoves(
+            applyMove(
+              move.piece[0],
+              move.piece[1],
+              target[0],
+              target[1],
+              checkLegalMoves(move.piece[0], move.piece[1], board.value, true),
+              board.value
+            ),
+            botPlayer.value == 1 ? 2 : 1
+          )
+        );
+        move.targets = move.targets.filter(t => !enemyMoves.find(m => t[0] == m[0] && t[1] == m[1]));
+      }
     }
   }
-  takeBlunders.filter(m => m.targets.length > 0);
+  takeBlunders = takeBlunders.filter(m => m.targets.length > 0);
 
   //take queen if possible
   let QueenTakes: Move[] = [];
-  for (let position of moveableBotPieces) {
-    let targets = checkLegalMoves(position[0], position[1], board.value, true).filter(m => board.value[m[0]][m[1]].type == 'Queen');
-    if (targets.length) QueenTakes.push({ piece: position, targets: targets });
+  {
+    for (let position of moveableBotPieces) {
+      let targets = checkLegalMoves(position[0], position[1], board.value, true).filter(m => board.value[m[0]][m[1]].type == 'Queen');
+      if (targets.length) QueenTakes.push({ piece: position, targets: targets });
+    }
   }
 
   //checkmate if possible
   let checkmate = {} as ReturnMove;
-  for (let piece of moveableBotPieces) {
-    for (let target of checkLegalMoves(piece[0], piece[1], board.value, true)) {
-      if (
-        checkAllLegalMoves(
+  {
+    for (let piece of moveableBotPieces) {
+      for (let target of checkLegalMoves(piece[0], piece[1], board.value, true)) {
+        if (
+          checkAllLegalMoves(
+            applyMove(piece[0], piece[1], target[0], target[1], checkLegalMoves(piece[0], piece[1], board.value, true), board.value),
+            botPlayer.value == 1 ? 2 : 1
+          ).length == 0
+        ) {
+          checkmate = { piece, target };
+        }
+      }
+    }
+  }
+  //find random checks
+  let checks: ReturnMove[] = [];
+  {
+    let viableMoves: Move[] = [];
+    for (let piece of moveableBotPieces) {
+      viableMoves.push({ piece, targets: checkLegalMoves(piece[0], piece[1], board.value, true) });
+      for (let target of checkLegalMoves(piece[0], piece[1], board.value, true)) {
+        let coverdFields = checkAllLegalMoves(
           applyMove(piece[0], piece[1], target[0], target[1], checkLegalMoves(piece[0], piece[1], board.value, true), board.value),
           botPlayer.value == 1 ? 2 : 1
-        ).length == 0
-      ) {
-        checkmate = { piece, target };
+        ).filter(m => m[0] == target[0] && m[1] == target[1]);
+
+        viableMoves = viableMoves.filter(m => m.targets.filter(t => !coverdFields.find(f => f[0] == t[0] && f[1] == t[1])));
+      }
+    }
+    for (let move of viableMoves) {
+      for (let target of move.targets) {
+        if (
+          checkChecks(
+            botPlayer.value == 1 ? 2 : 1,
+            applyMove(
+              move.piece[0],
+              move.piece[1],
+              target[0],
+              target[1],
+              checkLegalMoves(move.piece[0], move.piece[1], board.value, true),
+              board.value
+            )
+          ).length
+        ) {
+          checks.push({ piece: move.piece, target });
+        }
       }
     }
   }
@@ -189,12 +232,27 @@ function getGoodBotMove(moveableBotPieces: Position[]) {
   } else if (takeBlunders.length && takeBlunders[0].targets[0]) {
     returnMove = { piece: takeBlunders[0].piece, target: takeBlunders[0].targets[0] };
     console.log('takeBlunder', takeBlunders);
+  } else if (checks.length) {
+    returnMove = checks[0];
+    console.log('check');
   }
-  //get random move if nothing else works //TODO: update the if for every prio above
+  //get random move without blunders if nothing else works //TODO: update the if for every prio above
   else {
-    let position = moveableBotPieces[Math.floor(Math.random() * moveableBotPieces.length)];
-    let legalBotMoves = checkLegalMoves(position[0], position[1], board.value, true);
-    let target = legalBotMoves[Math.floor(Math.random() * legalBotMoves.length)];
+    let viableMoves: Move[] = [];
+    for (let piece of moveableBotPieces) {
+      viableMoves.push({ piece, targets: checkLegalMoves(piece[0], piece[1], board.value, true) });
+      for (let target of checkLegalMoves(piece[0], piece[1], board.value, true)) {
+        let coverdFields = checkAllLegalMoves(
+          applyMove(piece[0], piece[1], target[0], target[1], checkLegalMoves(piece[0], piece[1], board.value, true), board.value),
+          botPlayer.value == 1 ? 2 : 1
+        ).filter(m => m[0] == target[0] && m[1] == target[1]);
+
+        viableMoves = viableMoves.filter(m => m.targets.filter(t => !coverdFields.find(f => f[0] == t[0] && f[1] == t[1])));
+      }
+    }
+    let move = viableMoves[Math.floor(Math.random() * viableMoves.length)];
+    let position = move.piece;
+    let target = move.targets[Math.floor(Math.random() * move.targets.length)];
     returnMove = { piece: position, target: target };
     console.log('RandomMove');
   }
