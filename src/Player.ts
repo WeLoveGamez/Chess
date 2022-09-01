6b738c71a72847de2df7125b4b324fcd97b5e216
TimL135 <t.littschwager@mbd-team.de>
2022-09-01 19:32:32 +0200

    piece are unlockable with lvl

--- src/Player.ts
+++ src/Player.ts
@@ -14,3 +14,24 @@ if (!player.value) {
     lineup: { frontline: ['', 'Pawn', ''], backline: ['Pawn', 'King', 'Pawn'] },
   };
 }
+
+export function lvlUp() {
+  player.value.exp -= player.value.lvl * 10;
+  player.value.lvl++;
+  if (player.value.lvl >= 2) {
+    if (!player.value.units.find(e => e.name == 'Rook'))
+      player.value.units.push({ name: 'Rook', value: 5, maxAmount: 0, amount: 0, amountPerRound: 0 });
+  }
+  if (player.value.lvl >= 5) {
+    if (!player.value.units.find(e => e.name == 'Bishop'))
+      player.value.units.push({ name: 'Bishop', value: 3, maxAmount: 0, amount: 0, amountPerRound: 0 });
+  }
+  if (player.value.lvl >= 9) {
+    if (!player.value.units.find(e => e.name == 'Knight'))
+      player.value.units.push({ name: 'Knight', value: 3, maxAmount: 0, amount: 0, amountPerRound: 0 });
+  }
+  if (player.value.lvl >= 14) {
+    if (!player.value.units.find(e => e.name == 'Queen'))
+      player.value.units.push({ name: 'Queen', value: 9, maxAmount: 0, amount: 0, amountPerRound: 0 });
+  }
+}
